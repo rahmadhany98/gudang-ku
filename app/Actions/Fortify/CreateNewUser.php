dcdb47761dd2b1d0331504b520dc2d0c198645a2
Javiera Rahmadhany <kazuha004@gmail.com>
2023-06-13 13:05:20 +0800

    configure spatie for rbac

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -32,10 +32,16 @@ class CreateNewUser implements CreatesNewUsers
             'password' => $this->passwordRules(),
         ])->validate();
 
-        return User::create([
+        $role = Role::where('name', 'Customer')->first();
+
+        $user = User::create([
             'name' => $input['name'],
             'email' => $input['email'],
             'password' => Hash::make($input['password']),
         ]);
+
+        $user->assignRole($role);
+
+        return $user;
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -9,10 +9,11 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable, HasScope;
+    use HasApiTokens, HasFactory, Notifiable, HasScope, HasRoles;
 
     /**
      * The attributes that are mass assignable.
